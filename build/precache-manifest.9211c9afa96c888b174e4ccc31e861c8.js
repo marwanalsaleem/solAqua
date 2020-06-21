ef54032eaf88109d32096a3ba033d146b327505c
marwanalsaleem <marwanalsaleem11@gmail.com>
2020-06-21 01:49:37 +0000

    '1'

---
+++ build/precache-manifest.9211c9afa96c888b174e4ccc31e861c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2d2facd4ba4d5fbfee0986de5320ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2f894f82d2f4360fc32",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6396c10b6adcc00bc758",
+    "url": "/static/js/2.cc9b85ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc9b85ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f894f82d2f4360fc32",
+    "url": "/static/js/main.ab71658a.chunk.js"
+  },
+  {
+    "revision": "374c6747ca37c4bc118c",
+    "url": "/static/js/runtime-main.766fc875.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
